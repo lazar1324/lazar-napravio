1c189d0808f964eb14d01165db3aadb61065e95e
lazar1324 <97560248+lazar1324@users.noreply.github.com>
2022-01-11 22:33:02 +0100

    Add files via upload

---
+++ artikli.js
@@ -0,0 +1,5 @@
+ function izracunajArtikal(){
+            var a = document.getElementById("a").value;
+            var b = document.getElementById("b").value;
+            alert ("Uzeli ste " + (parseInt(a) + parseInt(b)) + " artikla.");
+        }       
